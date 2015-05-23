99f155f7417eb74d51cda309298a966b00d50b2c
farseer90718 <farseer90718@gmail.com>
2015-05-23 14:56:04 +0800

    spacemacs

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -381,7 +381,7 @@ volumewidget = lain.widgets.alsa({
 neticon = wibox.widget.imagebox(beautiful.widget_net)
 netwidget = wibox.widget.background(lain.widgets.net({
     settings = function()
-        widget:set_markup(markup.font("Monofur 12",
+        widget:set_markup(markup.font("NovaMono 11",
         markup("#7AC82E", " " .. string.format("%05.1f", net_now.received))
         .. " " ..
         markup("#46A8C3", " " .. string.format("%05.1f", net_now.sent) .. " ")))
