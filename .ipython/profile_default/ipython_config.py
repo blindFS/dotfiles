675fcd40f63c84231e7d9b53bb47a0ec41cb3936
blindFS <farseer90718@gmail.com>
2015-08-23 16:03:36 +0800

    ipython notebook mathjax

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -17,6 +17,10 @@ c.InteractiveShellApp.exec_lines = []
 c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
 c.InteractiveShellApp.exec_lines.append('%autoreload 2')
 
+### notebook
+
+c.NotebookApp.mathjax_url = '/usr/share/mathjax/MathJax.js'
+
 #------------------------------------------------------------------------------
 # InteractiveShellApp configuration
 #------------------------------------------------------------------------------
