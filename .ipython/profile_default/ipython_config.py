2cc9fb2df63922589289ce3006ec2bb4e6c90356
blindFS <farseer90718@gmail.com>
2015-08-23 16:42:52 +0800

    jupyter notebook

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -17,10 +17,6 @@ c.InteractiveShellApp.exec_lines = []
 c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
 c.InteractiveShellApp.exec_lines.append('%autoreload 2')
 
-### notebook
-
-c.NotebookApp.mathjax_url = '/usr/share/mathjax/MathJax.js'
-
 #------------------------------------------------------------------------------
 # InteractiveShellApp configuration
 #------------------------------------------------------------------------------
